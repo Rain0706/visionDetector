c899daaea0eb00deb653702753fb8271505eaef9
Rain0706 <rainhsu0706@gmail.com>
2026-03-09 23:03:34 +0800

    The first project I upload!

---
+++ yoloTest/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace yoloTest
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ yoloTest/MauiProgram.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using CommunityToolkit.Maui;
+
+namespace yoloTest
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .UseMauiCommunityToolkitCamera() // 2. 鏈接相機工具組初始化
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
